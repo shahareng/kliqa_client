b8ce3a71b9f876d31b5790a66d6aeb38da59e7df
ShaharEngel <shaharbel0@gmail.com>
2025-07-14 10:04:18 +0300

    remove events files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import Layout from "./Layout"
-import UserCard from "./pages/Admin/UsersCards/index.jsx";
-import Events from "./pages/Admin/Events/index.jsx";
-import React from "react";
+
 function App() {
 
   return (
     <div>
       <Layout />
-      {/* <UserCard /> */}
-      <Events />
     </div>
   )
 }
