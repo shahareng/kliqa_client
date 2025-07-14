2b3e29ac2c7d5483e06ec55cfd0f83be74271499
ShaharEngel <shaharbel0@gmail.com>
2025-07-14 18:24:22 +0300

    unNote userCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import React from "react";
 import Layout from "./Layout";
-
 
 function App() {
 
-  return (
-    <div>
-      <Layout />
-    </div>
-  )
+  return <Layout />
+};
 
-export default App;
+export default App
