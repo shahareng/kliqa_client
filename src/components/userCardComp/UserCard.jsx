6bd0afff0fe120dc13af605a1db83a9068cba067
ShaharEngel <shaharbel0@gmail.com>
2025-07-15 12:17:04 +0300

    design users card component

--- src/components/userCardComp/UserCard.jsx
+++ src/components/userCardComp/UserCard.jsx
@@ -4,10 +4,14 @@ import styles from "./style.module.css";
 function UserCard({ name, occupation, imageUrl, additionalInfo }) {
   return (
     <div className={styles.card}>
-      <img src={imageUrl} alt={name} className={styles.avatar} />
-      <h2 className={styles.name}>{name}</h2>
-      <p className={styles.occupation}>{occupation}</p>
-      <p className={styles.additionalInfo}>{additionalInfo}</p>
+      <div className={styles.imgBack}>
+        <img src={imageUrl} alt={name} className={styles.avatar} />
+      </div>
+      <div className={styles.content}>
+        <h2 className={styles.name}>{name}</h2>
+        <p className={styles.occupation}>{occupation}</p>
+        <p className={styles.additionalInfo}>{additionalInfo}</p>
+      </div>
     </div>
   );
 }
