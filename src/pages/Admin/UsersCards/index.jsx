c1ae583d202967c13a84420c3b216ebf23b1a396
ShaharEngel <shaharbel0@gmail.com>
2025-07-16 14:09:13 +0300

    start build search users by groups select

--- src/pages/Admin/UsersCards/index.jsx
+++ src/pages/Admin/UsersCards/index.jsx
@@ -4,6 +4,7 @@ import styles from "./style.module.css";
 import UserCard from "../../../components/userCardComp/UserCard";
 import SearchInput from "../../../components/SearchInput/index";
 import useApi from "../../../hooks/useApi";
+import SearchComp from "../../../components/SearchComp";
 
 const img = "https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg";
 
@@ -61,17 +62,24 @@ const img = "https://static.vecteezy.com/system/resources/previews/009/292/244/n
 
 function UsersCards() {
   const [search, setSearch] = useState("");
+  const [selectedGroups, setSelectedGroups] = useState([]);
 
   let { data, loading, error, callApi: fetchUser } = useApi(`/users`)
+  let { data: groups, loading: loadGroups, error: errorgroups, callApi: fetchgroups } = useApi(`/groups`)
 
   useEffect(() => {
     fetchUser()
-      // .then(res => setUser(res))
-      .catch(err => console.error("שגיאה ב‑GET משתמשים:", err));
+      .catch(err => console.error("get users error", err));
+
+    fetchgroups()
+      .catch(err => console.error("get groups error", err));
+
   }, []);
 
-  if (loading) return <div>Loading...</div>
+  if (loading || loadGroups) return <div>Loading...</div>
   if (error) return error
+
+  const groupsNames = groups.map(u => ({ value: u.name, label: u.name }));
 
   const filteredUsers = data.filter((user) =>
     `${user.first_name} ${user.last_name} ${user.JobsHistories[0]?.job_title || "looking for job"} ${user.additional_info}`
@@ -79,29 +87,58 @@ function UsersCards() {
       .includes(search.toLowerCase())
   );
 
+  // const handleSelect = (option) => {
+  //   setSelectedGroups(option);
+  //   console.log("נבחר:", option);
+  // };
+
+  const handleSelect = (name, selected) => {
+    console.log("selected", selected);
+    // setSelectedGroups(selected.map(s => s.value))
+    // groupsNames.filter(o =>
+    //   selectedGroups?.some(g => g.name === o.value));
+    setSelectedGroups(prev => [...prev, selected[0].value])
+    console.log("sel1 ", selected.map(s => s.value));
+    console.log("sel ", selectedGroups);
+  };
+
+  // const handleSelect = (name, selected) => {
+  // setSelectedGroups(prev => ({
+  //   ...prev,
+  //   [name]: selected.map(s =>
+  //     name === 'groups'
+  //       ? { group_id: s.value, name: s.value }
+  //       : { contribution_id: s.value, type: s.value }
+  //   )
+  // }));
+  // setSelectedGroups.push()
+  // };
+
+  // const selectedGroups = groupsNames.filter(o =>
+  //   user.groups.some(g => g.name === o.value)
+  // );
+
   return (
     <div className={styles.container}>
-      <div className={styles.titles}>
+      <div className={styles.content}>
         <h1 className={styles.pageTitle}>All Members</h1>
-        <SearchInput
-          value={search}
-          onChange={setSearch}
-          placeholder="Search users..."
-        />
+        <div className={styles.grid}>
+          {filteredUsers.map((user, idx) => (
+            <UserCard
+              key={idx}
+              id={user.id}
+              name={`${user.first_name} ${user.last_name}`}
+              occupation={user.JobsHistories[0]?.job_title || "looking for job"}
+              imageUrl={img || user.profile_picture}
+              additionalInfo={user.additional_info}
+            />
+          ))}
+        </div>
       </div>
-
-      <div className={styles.grid}>
-        {filteredUsers.map((user, idx) => (
-          <UserCard
-            key={idx}
-            id={user.id}
-            name={`${user.first_name} ${user.last_name}`}
-            occupation={user.JobsHistories[0]?.job_title || "looking for job"}
-            imageUrl={img || user.profile_picture}
-            additionalInfo={user.additional_info}
-          />
-        ))}
-      </div>
+      {loadGroups ? 'loading...' :
+        <SearchComp
+          search={search} setSearch={setSearch} placeholder={"Search users..."}
+          name={"Groups"} options={groupsNames} handleSelect={handleSelect} selected={selectedGroups} />}
     </div>
   );
 }
