ac3fb6fc53c4f05bd998252e85eb504b2b2ed677
ShaharEngel <shaharbel0@gmail.com>
2025-07-16 16:12:29 +0300

    merge changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import Layout from "./Layout";
-import LinkedInPage from "./pages/LinkedinPage/linkedInPage.jsx";
-import AdminImportSection from "./components/UploadExcelCV/AdminImportSection";
 
 
 function App() {
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-        {/*<Layout />*/}
-        {/*<LinkedInPage />*/}
-       <h1>מערכת ניהול משתמשים</h1>
-        <AdminImportSection />
-    </div>
+    <Layout />
   );
 };
 
