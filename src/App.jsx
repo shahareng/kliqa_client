57e7358982bc3b3da65c618b52fae0186a99742b
Maayan Simkins <atia.maayan@gmail.com>
2025-07-16 13:29:44 +0300

    arragne folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import Layout from "./Layout";
+import LinkedInPage from "./pages/LinkedinPage/linkedInPage.jsx";
+import AdminImportSection from "./components/UploadExcelCV/AdminImportSection";
+
 
 function App() {
 
-  return <Layout />
+  return (
+    <>
+      <Layout />
+      <LinkedInPage />
+     <h1>מערכת ניהול משתמשים</h1>
+      <AdminImportSection />
+    </>
+  );
 };
 
 export default App
--- src/components/UploadExcelCV/UploadExcelBtn/index.jsx
+++ src/components/UploadExcelCV/UploadExcelBtn/index.jsx
@@ -1,22 +1,26 @@
 import React, { useRef } from 'react';
 
-// props:
-// onFileSelect: פונקציה שהורה מעביר, שמופעלת כשנבחר קובץ
 function UploadExcelBtn({ onFileSelect }) {
   const inputRef = useRef(null);
 
-  // פותח דיאלוג קבצים
+  // פותח את דיאלוג בחירת הקובץ
   const handleButtonClick = () => {
     inputRef.current.click();
   };
 
-  // נבחר קובץ
+  // ברגע שנבחר קובץ
   const handleFileChange = (event) => {
     const file = event.target.files[0];
-    if (file && (file.type === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" || file.type === "application/vnd.ms-excel")) {
-      onFileSelect(file); // מעביר להורה
+    if (
+      file &&
+      (
+        file.type === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
+        file.type === "application/vnd.ms-excel"
+      )
+    ) {
+      onFileSelect(file); // מדווח להורה
     } else {
-      alert("יש לבחור קובץ אקסל בלבד (xls, xlsx)");
+      alert("נא לבחור קובץ אקסל בלבד (xls, xlsx)");
       event.target.value = null;
     }
   };
@@ -24,14 +28,14 @@ function UploadExcelBtn({ onFileSelect }) {
   return (
     <div>
       <button type="button" onClick={handleButtonClick}>
-        העלה קובץ אקסל
+        העלאת קובץ אקסל
       </button>
       <input
         type="file"
         accept=".xlsx,.xls"
-        style={{ display: "none" }}
         ref={inputRef}
         onChange={handleFileChange}
+        style={{ display: 'none' }}
       />
     </div>
   );
